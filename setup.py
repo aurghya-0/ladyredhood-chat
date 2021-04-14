ed8b010f42c845db2c46b5a2ffa4c5029d48026d
aurghya-0 <aurghya.blog@gmail.com>
2021-04-15 00:34:57 +0530

    added files

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
 from setuptools import setup
 
-with open('DESCRIPTION.txt') as file:
-    long_description = file.read()
-
-
 REQUIREMENTS =['chat_downloader', 'click']
 
 CLASSIFIERS = [
     'Development Status :: 4 - Beta',
-    'Intended Audience :: Streamers',
-    'Topic :: YouTube',
-    'License :: MIT License',
+    'Intended Audience :: Developers',
+    'Topic :: Internet',
+    'License :: OSI Approved :: MIT License',
     'Programming Language :: Python'
 ]
 
@@ -18,10 +14,10 @@ setup(
     name='ladyredhood-chats',
     version='0.1.0',
     description='A small script to extract timestamps from youtube chats',
-    long_description=long_description,
     author='Aurghyadip Kundu',
     author_email='director@webedutech.org',
     license='MIT',
+    url='https://github.com/aurghya-0/ladyredhood-chat',
     packages=['chats'],
     classifiers=CLASSIFIERS,
     install_requires=REQUIREMENTS,
